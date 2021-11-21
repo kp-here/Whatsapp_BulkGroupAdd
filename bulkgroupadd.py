1572d05e63901237d0eaf9a64fbb9a2a7f7d878e
Abhiram K P <73926551+kp-here@users.noreply.github.com>
2021-11-22 03:33:14 +0530

    Create bulkgroupadd.py

---
+++ bulkgroupadd.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+grp_name = input("\nEnter Group Name :\n")
+
+#importing contact names
+l=[]
+with open("names.csv") as file:
+    for a in file.readlines():
+        l.append(a.rstrip('\n'))
+file.close()
+
+#loading web driver
+driver = webdriver.Chrome(executable_path=r'T:\Whatsapp Automation\chromedriver.exe')
+wait = WebDriverWait(driver, 600)
+driver.maximize_window()
+driver.get('https://web.whatsapp.com/')
+
+
+#selenium functions to select and click elements
+def findbyxpath(x):
+    a= wait.until(EC.presence_of_element_located((By.XPATH, x)))
+    a.click()
+    return a
+
+def findbyxpath_noclick(x):
+    return wait.until(EC.presence_of_element_located((By.XPATH, x)))
+
+#click search box
+group_title = findbyxpath("//div[@class='_13NKt copyable-text selectable-text']")
+time.sleep(1)
+#type in group name and select it
+group_title.send_keys(grp_name + Keys.ENTER) 
+time.sleep(1)
+
+#clicked on group name header
+head_title= findbyxpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[4]/div[1]/header[1]/div[2]/div[1]/div[1]/span[1]")
+time.sleep(1)
+
+#click on add participant
+add_part=findbyxpath("//div[contains(text(),'Add participant')]")
+time.sleep(1)
+
+#select participants one by one
+for i in l:
+    box=findbyxpath_noclick("//div[@class='nBIOd tm2tP copyable-area']//div//div[@class='_13NKt copyable-text selectable-text']")
+    box.send_keys(i)
+    time.sleep(1)
+    box.send_keys(Keys.ENTER)
+
+#confirm the selected participants [green tick]
+finalize=findbyxpath("//div[@class='_165_h _2HL9j']") 
+time.sleep(1)
+
+#confirm - 'add participant'
+confirm = findbyxpath("//div[@class='tvf2evcx m0h2a7mj lb5m6g5c j7l1k36l ktfrpxia nu7pwgvd gjuq5ydh'][contains(text(),'Add participant')]") 
+time.sleep(1)
+
+#invite to group - for participants who cannot be added
+invite=findbyxpath("//div[@class='tvf2evcx m0h2a7mj lb5m6g5c j7l1k36l ktfrpxia nu7pwgvd gjuq5ydh'][contains(text(),'Invite to group')]") 
+time.sleep(1)
+
+#'send invite'
+invite=findbyxpath("//div[@class='_165_h _2HL9j']") 
+
+
